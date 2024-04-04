938838b28ca46dab70c0c407882ce5e1aaba0a6b
Meg Lee <megleedev@outlook.com>
2024-04-04 01:55:20 -0700

    map.png and map.json updated -- added html and css for styling -- began setup for map, player, and textbox

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx";
 
 k.loadSprite ("spritesheet", "./spritesheet.png", {
@@ -10,6 +11,67 @@ k.loadSprite ("spritesheet", "./spritesheet.png", {
         "idle-side": 975,
         "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
         "idle-up": 1014,
-        "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 }
+        "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
+        "bag-open": 128
     }
 });
+
+k.loadSprite ("map", "./map.png");
+
+k.setBackground (k.Color.fromHex ("#311047"));
+
+/* Creating a scene*/
+
+k.scene ("main", async () => {
+
+    const mapData = await (await fetch ("./map.json")).json ();
+    const layers = mapData.layers;
+
+    const map = k.make ([
+        k.sprite ("map"),
+        k.pos (0),
+        // Controls the scale size of the map -- see constants.js
+        k.scale (scaleFactor)
+    ]);
+
+    const player = k.make ([
+        k.sprite ("spritesheet", { anim: "idle-down" }),
+        k.area ([ 
+            shape, new k.Rect (k.vec2(0, 3), 10, 10),
+        ]),
+        k.body (),
+        k.anchor ("center"),
+        k.pos (),
+        k.scale (scaleFactor),
+        {
+            speed: 250,
+            direction: "down",
+            isInDialog: false,
+        },
+        "player",
+    ]);
+
+    for (const layer of layers) {
+        if (layer.name == "Boundaries") {
+            for (const boundary of layer.objects) {
+                map.add ([
+                    k.area ({
+                        shape: new k.Rect (k.vec2(0), boundary.width, boundary.height),
+                    }),
+                    k.body ([isStatic = true]),
+                    k.pos (boundary.x, boundary.y),
+                    boundary.name,
+                ]);
+
+                if (boundary.name) {
+                    player.onCollide(boundary.name, () => {
+                        player.isInDialog = true;
+                        // TODO: Add dialog logic
+                    })
+                }
+            }
+        }
+    }
+});
+
+k.go ("main");
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export function displayDialog (text, onDisplayEnd) {
+    const dialogUI = document.getElementById("textbox-container");
+}
+
+// TODO: Start here when picking up the project again. 1:11:31 for references.
